812afe1ccc0547f8be9f90b31b3b930f5f60577a
Senbonzakura1493 <fatine.benmoussa@icloud.com>
2021-08-26 07:11:43 +0200

    deleting unuseful component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 import {MatDialog} from '@angular/material/dialog';
-import {BasketComponent} from '../shop/basket/basket.component';
 import {Overlay} from '@angular/cdk/overlay';
 @Component({
   selector: 'app-navbar',
@@ -18,37 +17,7 @@ export class NavbarComponent implements OnInit {
 
   }
  
-  //fonctions d'ouverture des modals
-  openDialog(mobile?): void {
-
-    if(this.opencart == true ){
-      this.dialog.closeAll()
-      this.opencart = false;
-    }
-    else{
-      if(mobile == true ){
-        let dialogRef = this.dialog.open(BasketComponent, {
-          height :"100%",
-          width :"100%",
-          panelClass:['mat-dialog-color','mat-dialog-color-mobile'],
-          position: { right: '0'},
-          autoFocus : false,
-        },);
-      }
-      else{
-      let dialogRef = this.dialog.open(BasketComponent, {
-        height :"100%",
-        width :"30%",
-        panelClass: 'mat-dialog-color',
-        position: { right: '0'},
-        autoFocus : false,
-      },);
-    }
   
-      this.opencart = true ;
-    }
-    
-    
-  }
+  
 
 }
